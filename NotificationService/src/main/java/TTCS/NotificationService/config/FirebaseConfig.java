2b73ffe50abcf96ee67f74f232ca346afbddd06b
NvManhK18KMA <127221471+manhk18kma@users.noreply.github.com>
2024-06-29 14:32:27 +0700

    Commition 29-6

--- NotificationService/src/main/java/TTCS/NotificationService/config/FirebaseConfig.java
+++ NotificationService/src/main/java/TTCS/NotificationService/config/FirebaseConfig.java
@@ -1,30 +1,30 @@
-package TTCS.NotificationService.config;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
-
-@Configuration
-public class FirebaseConfig {
-    @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        // Load Firebase Admin SDK credentials from JSON file in resources
-        ClassPathResource serviceAccountFile = new ClassPathResource("notification-d53ec-firebase-adminsdk-p6dkb-c0bfdfedfb.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
-                .setDatabaseUrl("https://notification-d53ec-default-rtdb.firebaseio.com/")
-                .build();
-
-        FirebaseApp.initializeApp(options);
-
-        return FirebaseMessaging.getInstance();
-    }
-
-}
+//package TTCS.NotificationService.config;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.messaging.FirebaseMessaging;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import java.io.IOException;
+//
+//@Configuration
+//public class FirebaseConfig {
+//    @Bean
+//    FirebaseMessaging firebaseMessaging() throws IOException {
+//        // Load Firebase Admin SDK credentials from JSON file in resources
+//        ClassPathResource serviceAccountFile = new ClassPathResource("notification-d53ec-firebase-adminsdk-p6dkb-c0bfdfedfb.json");
+//
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
+//                .setDatabaseUrl("https://notification-d53ec-default-rtdb.firebaseio.com/")
+//                .build();
+//
+//        FirebaseApp.initializeApp(options);
+//
+//        return FirebaseMessaging.getInstance();
+//    }
+//
+//}
